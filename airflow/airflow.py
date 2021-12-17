f947da086ce0943f8a128390faac7bb31f2fcecf
lucas8585 <94440244+lucas8585@users.noreply.github.com>
2021-12-17 08:43:18 -0800

    Update airflow.py

--- airflow/airflow.py
+++ airflow/airflow.py
@@ -36,7 +36,7 @@ def myFunction():
 
     df.to_parquet('elections2022.parquet')
 
- 
+ #On a crée un dag qui va nous permettre d'executer des tâches 
 
 with DAG(
 
@@ -50,7 +50,9 @@ with DAG(
 
 ) as dag:
 
- 
+ # Ici on a crée le dag et on va ensuite lui attribuer des tâches, ici en l'ocurrence on a décider de faire un ETL (Extract, Transform, Load)
+  # en tâche 1 on va récuperer la donnée précédemment traiter avec pyspark 
+  # en tâche 2 on va transformer cette donnée en parquet afin de la charger par la suite 
 
     task_1 = BashOperator(
 
@@ -71,5 +73,8 @@ with DAG(
     )
 
  
-
+# le .set_downstream nous permet ici de donner l'orde à laquel les tâches vont fonctionner. Par exemple ici la task_1 va s'executer 
+# et quand elle aura fini cela lancera l'éxecution de la task_2 on peut aussi remplacer le .set_downstream par >>
 task_1.set_downstream(task_2)
+
+#Malheureusement, on a eu un soucis avec airflow qui n'a pas afficher notre dag, on ne pouvait donc pas obtenir notre fichier résultat en .parquet
